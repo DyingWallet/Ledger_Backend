6b05880e4b967ac5cbeb116b39cab421adf6e26e
XuRonghao <ronghao.xu@enmotech.com>
2021-05-27 01:44:41 +0800

    refactor

--- src/main/java/stu/xuronghao/ledger/service/imp/AdminServiceImp.java
+++ src/main/java/stu/xuronghao/ledger/service/imp/AdminServiceImp.java
@@ -2,7 +2,7 @@ package stu.xuronghao.ledger.service.imp;
 
 
 import stu.xuronghao.ledger.entity.*;
-import stu.xuronghao.ledger.mapper.AdminDao;
+import stu.xuronghao.ledger.mapper.AdminMapper;
 import stu.xuronghao.ledger.service.AdminService;
 import org.springframework.stereotype.Service;
 
@@ -13,26 +13,26 @@ import java.util.List;
 public class AdminServiceImp implements AdminService {
 
     @Resource
-    private AdminDao dao;
+    private AdminMapper mapper;
 
     @Override
     public List<User> queryAllUser() {
-        return dao.findAllUser();
+        return mapper.findAllUser();
     }
 
     @Override
     public List<User> queryByKey(String key) {
-        return dao.findUserByKey(key);
+        return mapper.findUserByKey(key);
     }
 
     @Override
     public List<Anno> queryAnnounce(String key) {
-        return dao.queryAnnounce(key);
+        return mapper.queryAnnounce(key);
     }
 
     @Override
     public List<Feedback> queryAllFeedback() {
-        return dao.findAllFeedback();
+        return mapper.findAllFeedback();
     }
 
     @Override
@@ -42,41 +42,41 @@ public class AdminServiceImp implements AdminService {
 
     @Override
     public void updateUserStatus(String id) {
-        dao.updateUserStatus(id);
+        mapper.updateUserStatus(id);
         return;
     }
 
     @Override
     public void processedFeedback(String id) {
-        dao.processedFeedback(id);
+        mapper.processedFeedback(id);
         return;
     }
 
     @Override
     public void revocationAnnounce(String id) {
-        dao.revocationAnnounce(id);
+        mapper.revocationAnnounce(id);
         return;
     }
 
     @Override
     public void deleteAnnounce(String id){
-        dao.deleteAnnounce(id);
+        mapper.deleteAnnounce(id);
     }
 
     @Override
     public Admin login(String id, String password) {
-        Admin admin = dao.adminLogin(id, password);
+        Admin admin = mapper.adminLogin(id, password);
         return admin;
     }
 
     @Override
     public void adminUpdatePasswd(String newPasswd, String adminNo) {
-        dao.adminUpdatePasswd(newPasswd, adminNo);
+        mapper.adminUpdatePasswd(newPasswd, adminNo);
         return;
     }
 
     @Override
     public void postAnnounce(String title, String type, String content, String date, String adminNo) {
-        dao.insertAnnounce("123", title, type, content, date, adminNo);
+        mapper.insertAnnounce("123", title, type, content, date, adminNo);
     }
 }
