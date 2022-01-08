fb3f087155153e1f164ef047e0daa08fe366e510
DyingWallet <709038511@qq.com>
2022-01-08 10:06:18 +0800

    Web端Filter重定向问题，添加直接访问根目录时的登录页跳转

--- src/main/java/stu/xuronghao/ledger/controller/filter/AdminLoginFilter.java
+++ src/main/java/stu/xuronghao/ledger/controller/filter/AdminLoginFilter.java
@@ -1,6 +1,7 @@
 package stu.xuronghao.ledger.controller.filter;
 
-import stu.xuronghao.ledger.utils.ConstantVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -10,23 +11,24 @@ import javax.servlet.http.HttpServletResponseWrapper;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-//@ServletComponentScan
-//@Component("securityRequestFilter")
-@WebFilter(urlPatterns = "/Admin/*", filterName = "securityRequestFilter")
+@WebFilter(urlPatterns = {"/Admin/*","/"}, filterName = "securityRequestFilter")
 
 public class AdminLoginFilter implements Filter {
+
+    private static final Logger log = LoggerFactory.getLogger(AdminLoginFilter.class);
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("init filter");
+        log.info("init filter");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("doFilter");
+        log.info("doFilter");
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpSession session = req.getSession();
         HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper((HttpServletResponse) servletResponse);
-        System.out.println(req.getRequestURI());
+        log.info(req.getRequestURI());
         //登录则放行
         if ((req.getRequestURI().contains("login") || session.getAttribute("admin") != null || req.getRequestURI().contains("/adminLogin")) || (req.getRequestURI().contains("/img/"))) {
             filterChain.doFilter(servletRequest, servletResponse);
